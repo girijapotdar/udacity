6a4b468700bd4c89bb680ce4f45318ad9e5fad23
Girija Harkare <girija.potdar@gmail.com>
2019-03-04 01:52:19 -0800

    Incorporating comments

--- Memory Game/fend-project-memory-game-master/js/app.js
+++ Memory Game/fend-project-memory-game-master/js/app.js
@@ -1,16 +1,24 @@
-/*
- * Create a list that holds all of your cards
- */
+//Variable declaration
+let previousOpenCard;
+let twoCardsOpen = false;
+let listOfCards;
+let moves = 0;
+let numberOfMatchedCards = 0;
+let noOfStars = 3;
+let dt = null;
+let hours;
+let mins;
+let secs;
+let stopTimer = true;
+let isTimerStarted = false;
+let timer = document.getElementById("timer");
+document.querySelector('.moves').innerHTML = moves;
+
+//Call to start the game
+init();
 
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
-// Shuffle function from http://stackoverflow.com/a/2450976
+// Shuffle the list of cards 
 function shuffle(array) {
     var currentIndex = array.length,
         temporaryValue, randomIndex;
@@ -26,21 +34,7 @@ function shuffle(array) {
     return array;
 }
 
-
-
-let previousOpenCard;
-let twoCardsOpen = false;
-let listOfCards;
-let moves = 0;
-let numberOfMatchedCards = 0;
-let noOfStars = 3;
-let dt = null;
-let hours;
-let mins;
-let secs;
-let stopTimer = true;
-let timer = document.getElementById("timer")
-
+//Start the timer
 function setTime() {
     if (dt == null) {
         dt = new Date();
@@ -51,12 +45,10 @@ function setTime() {
     secs = parseInt(secFromStart - (hours * 60 * 60) - (mins * 60));
     timer.value = appendZero(hours) + ":" + appendZero(mins) + ":" + appendZero(secs);
 
-    if (!stopTimer) {
+    if (isTimerStarted) {
         setTimeout(setTime, 1000);
     }
 }
-document.querySelector('.moves').innerHTML = moves;
-init();
 
 
 let restart = document.querySelector('.restart');
@@ -68,25 +60,34 @@ function appendZero(val) {
     return (val > 9 ? val : ('0' + val));
 }
 
+//Function to start a new game
 function init() {
     listOfCards = Array.from(document.querySelectorAll('.card'));
-    attachEvents();
-    stopTimer = false;
-    dt = null;
-    setTime();
-
-}
-
-function restartGame() {
-    resetCards();
+    //added shuffle in init code to reshuffle the game every time it starts
     shuffle(listOfCards);
     listOuterHtml = listOfCards.map(function(card) {
         return card.outerHTML;
     })
     document.querySelector('.deck').innerHTML = listOuterHtml.join('');
+    listOfCards = Array.from(document.querySelectorAll('.card'));
+    attachEvents();
+    isTimerStarted = false;
+    stopTimer = false;
+    dt = null;
+    setTime();
+}
+//On click of restart icon, the game resets and starts a new game
+function restartGame() {
+    resetCards();
+    // shuffle(listOfCards);
+    // listOuterHtml = listOfCards.map(function(card) {
+    //     return card.outerHTML;
+    // })
+    // document.querySelector('.deck').innerHTML = listOuterHtml.join('');
     init();
 }
 
+//Opens popup on win displaying the score(star rating, moves, duration)
 function openModalBox() {
     let modal = document.getElementById('myModal');
     let span = document.getElementsByClassName("close")[0];
@@ -106,22 +107,38 @@ function openModalBox() {
         }
     }
 }
-
+//Attach click event to list element in the DOM
 function attachEvents() {
     listOfCards.forEach(function(card) {
+        //set up the event listener for a card. If a card is clicked
         card.addEventListener('click', function(event) {
+            if (!isTimerStarted) {
+                dt = null;
+                isTimerStarted = true;
+                setTime();
+
+            }
+
+            //When the cards do not match
             if (!card.classList.contains('match') && !(card === previousOpenCard) && !twoCardsOpen) {
                 moves++;
                 starRating();
             }
             document.querySelector('.moves').innerHTML = moves;
+            //Return if 1: Check if trying to click the third card when two cards are open 
+            //2: Or Check if the same card is already clicked
+            //3: Or the matched card is clicked
+
             if (twoCardsOpen || card === previousOpenCard || card.classList.contains('match')) {
                 return;
             }
             card.classList.add('open', 'show');
+            //Check if the card clicked is a previous card
             if (previousOpenCard) {
                 let prevCardCopy = previousOpenCard;
                 twoCardsOpen = true;
+                //Check the classes of two opened cards. 
+                //If match then add the 'match' class, stop the timer, open win popup 
                 let previouOpenCardArray = Array.from(previousOpenCard.firstElementChild.classList);
                 let currentCardArray = Array.from(card.firstElementChild.classList);
                 let isMatch = arrayEquals(previouOpenCardArray, currentCardArray);
@@ -131,12 +148,15 @@ function attachEvents() {
                     twoCardsOpen = false;
                     numberOfMatchedCards += 2;
                     if (numberOfMatchedCards == listOfCards.length) {
+                        isTimerStarted = false;
                         stopTimer = true;
                         openModalBox();
                     }
+                    //If no match, add mismatch class to indicate user that there is a mismatch
                 } else {
                     card.classList.add('mismatch');
                     prevCardCopy.classList.add('mismatch');
+                    //after a second remove the classes applied 
                     setTimeout(function() {
                         card.classList.remove('open', 'show', 'mismatch');
                         prevCardCopy.classList.remove('open', 'show', 'mismatch');
@@ -151,7 +171,7 @@ function attachEvents() {
     });
 
 }
-
+//Calculate star rating depending on the moves
 function starRating() {
     if (moves > 40) {
         document.querySelector('.stars').children[2].style.display = 'none';
@@ -162,7 +182,7 @@ function starRating() {
         noOfStars = 2;
     }
 }
-
+// Reset cards to original state, clear moves and rating
 function resetCards() {
     listOfCards.forEach(function(card) {
         card.classList.remove('open', 'show', 'match', 'mismatch');
@@ -176,16 +196,7 @@ function resetCards() {
     document.querySelector('.moves').innerHTML = moves;
 }
 
+//Compare two array's length and values 
 function arrayEquals(a, b) {
     return (a.length === b.length && a.every((value, index) => value === b[index]));
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
